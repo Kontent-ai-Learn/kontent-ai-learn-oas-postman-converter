1b8f92cf2f77e0feaad0b4286b5e595f99980e53
Richard Sustek <ennspassion@gmail.com>
2023-02-27 13:25:58 +0100

    updates worker script

--- src/workerScript.ts
+++ src/workerScript.ts
@@ -1,29 +1,87 @@
-import {processSpecs} from './collectionProcessing';
-import {fetchSpecFiles} from './fetchSpecFiles';
-import {debugLog, prettify,} from './utils';
+import { processSpecs } from "./collectionProcessing";
+import { debugLog, prettify } from "./utils";
+import * as dotenv from "dotenv";
+import { GithubService } from "./githubService";
 
-const blobFolderUrl = process.env.BlobFolderUrl;
+// needed to load .env environment to current process when run via package.json script
+dotenv.config({
+  path: "./.env.local",
+});
+
+const accessToken = process.env.ApiReferenceGhAccessToken;
+const branch = process.env.ApiReferenceGhBranch;
+const email = process.env.ApiReferenceGhEmail;
+const owner = process.env.ApiReferenceGhOwner;
+const repository = process.env.ApiReferenceGhRepository;
+const username = process.env.ApiReferenceGhUsername;
+
+if (!accessToken) {
+  throw Error(`Invalid GH config: access token`);
+}
+
+if (!branch) {
+  throw Error(`Invalid GH config: branch`);
+}
+
+if (!email) {
+  throw Error(`Invalid GH config: email`);
+}
+
+if (!owner) {
+  throw Error(`Invalid GH config: owner`);
+}
+if (!repository) {
+  throw Error(`Invalid GH config: repository`);
+}
+
+if (!username) {
+  throw Error(`Invalid GH config: username`);
+}
+
+const githubService = new GithubService({
+  accessToken: accessToken,
+  branch: branch,
+  email: email,
+  owner: owner,
+  repository: repository,
+  username: username,
+});
+
+const apiReferenceFileNames = process.env.ApiReferenceFileNames;
+if (!apiReferenceFileNames) {
+  throw Error(
+    `Invalid API reference file names. Expected comma separated string with names of API reference files.`
+  );
+}
+
+const processFiles: string[] = apiReferenceFileNames
+  .split(",")
+  .map((m) => m.trim());
 
 export const work = async () => {
-  debugLog(`Blob folder Url is '${blobFolderUrl}'.`);
+  debugLog(
+    `Accessing GitHub: '${owner}/${repository}@${branch}' with user '${username}(${email})'`
+  );
 
-  const OAS_SPECS_FILE_NAMES = [
-    'delivery_api.json',
-    'management_api_v2.json',
-    'subscription_api.json',
-    'sync_api.json'
-  ];
+  const fetchedApiReferences: string[] = [];
 
-  const fileUrls = OAS_SPECS_FILE_NAMES.map(fileName => {
-    return `${blobFolderUrl}${fileName}`;
-  });
+  for (const file of processFiles) {
+    debugLog(`Fetching file '${file}'`);
 
-  try {
-    const specs = await fetchSpecFiles(fileUrls);
-    const collection = await processSpecs(specs);
+    const apiReferenceCode = await githubService.getApiReferenceCode({
+      filePath: `${file}`,
+    });
 
-    return prettify(collection);
-  } catch (error) {
-    return prettify(error);
+    if (apiReferenceCode) {
+      fetchedApiReferences.push(apiReferenceCode);
+    } else {
+      debugLog(`File '${file}' is empty or invalid`);
+    }
+
+    debugLog(`Successfully fetched '${file}'`);
   }
+
+  const collection = await processSpecs(fetchedApiReferences);
+
+  return prettify(collection);
 };
